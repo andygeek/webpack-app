aacac301ccba03728bd08e6eb29ce522bb4b557e
AndyGeek <andy.eulogio@gmail.com>
2020-09-18 17:04:58 -0500

    update

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./styles/index.css"
+var div = document.createElement("div")
+var text = document.createTextNode("AndyGeek")
+div.appendChild(text)
+document.body.appendChild(div)
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "../public/styles/[name].css",
+    }),
+    new HtmlWebpackPlugin({
+      title: "AndyGeek",
+    }),
+  ],
+};
